e8927872f60e4dcaa0d67a7f6b0dabf1c641b4af
Cade <ccurry@students.pitzer.edu>
2021-07-12 12:04:20 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+else:
+    app.debug = False
+
+# ---------------------------setup webapp structure/routing----------------------------------
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
